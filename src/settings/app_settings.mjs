63d437a6abf04272ff79306cb0b8943bca1915f9
Alvaro R Martin <exagonsoft@gmail.com>
2024-02-18 07:12:19 +0100

    #1

--- src/settings/app_settings.mjs
+++ src/settings/app_settings.mjs
@@ -1,6 +1,6 @@
 export const AppVariables = {
     AppDomain: "localhost",
-    AppPort: 3838,
+    AppPort: 8080,
     informalSourceUrl: "https://eltoque.com/tasas-de-cambio-de-moneda-en-cuba-hoy",
     formalSourceUrl: "https://www.bc.gob.cu",
     informalStorageFile: "informalData.json",
